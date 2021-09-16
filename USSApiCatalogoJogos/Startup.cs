0564481b7ba21043169dfc18b89972c083fb8666
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-09-16 01:31:42 -0300

    implementation of in-memory repository for development

--- USSApiCatalogoJogos/Startup.cs
+++ USSApiCatalogoJogos/Startup.cs
@@ -11,6 +11,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using USSApiCatalogoJogos.Repositories;
+using USSApiCatalogoJogos.Services;
 
 namespace USSApiCatalogoJogos
 {
@@ -26,6 +28,8 @@ namespace USSApiCatalogoJogos
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IJogoService, JogoService>();
+            services.AddScoped<IJogoRepository, JogoRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
