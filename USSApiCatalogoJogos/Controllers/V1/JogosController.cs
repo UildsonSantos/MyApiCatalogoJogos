45f7c360c18bf53cf9cf81c7922b1be26447e545
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-09-16 00:09:50 -0300

    created initial controller implementation

---
+++ USSApiCatalogoJogos/Controllers/V1/JogosController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using USSApiCatalogoJogos.InputModel;
+using USSApiCatalogoJogos.ViewModel;
+
+namespace USSApiCatalogoJogos.Controllers.V1
+{
+    [Route("api/V1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+          
+        [HttpGet]
+        public async Task<ActionResult<List<object>>> Obter()
+        {
+            return Ok();
+        }
+
+
+        [HttpGet("{idJogo:guid}")]
+        public async Task<ActionResult<JogoViewModel>> Obter(Guid idJogo)
+        {
+            return Ok();
+        }
+
+       
+        [HttpPost]
+        public async Task<ActionResult<JogoViewModel>> InserirJogo(JogoInputModel jogo)
+        {
+            return Ok();
+        }
+
+
+        [HttpPut("{idJogo:guid}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, JogoInputModel jogo)
+        {
+            return Ok();
+        }
+
+               
+        [HttpPatch("{idJogo:guid}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo(Guid idJogo, double preco)
+        {
+            return Ok();
+        }
+
+          
+        [HttpDelete("{idJogo:guid}")]
+        public async Task<ActionResult> ApagarJogo([FromRoute] Guid idJogo)
+        {
+            return Ok();
+        }
+    }
+}
