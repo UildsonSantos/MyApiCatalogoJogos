455a6613764d7dc2ffd2cb8258206203326450f8
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-09-16 00:39:04 -0300

    game repository contract defined

---
+++ USSApiCatalogoJogos/Repositories/IJogoRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using USSApiCatalogoJogos.Entities;
+
+namespace USSApiCatalogoJogos.Repositories
+{
+    public interface IJogoRepository
+    {
+        Task<List<Jogo>> Obter(int pagina, int quantidade);
+        Task<Jogo> Obter(Guid id);
+        Task<List<Jogo>> Obter(string nome, string produtora);
+        Task Inserir(Jogo jogo);
+        Task Atualizar(Jogo jogo);
+        Task Remover(Guid id);
+    }
+}
