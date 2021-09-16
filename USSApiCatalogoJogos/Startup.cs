66585857dc5cce57f04498b4bb735f825eee61a8
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-09-16 01:50:20 -0300

    implementation of real repository for production

--- USSApiCatalogoJogos/Startup.cs
+++ USSApiCatalogoJogos/Startup.cs
@@ -29,7 +29,7 @@ namespace USSApiCatalogoJogos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddScoped<IJogoService, JogoService>();
-            services.AddScoped<IJogoRepository, JogoRepository>();
+            services.AddScoped<IJogoRepository, JogoSqlServerRepository>();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
